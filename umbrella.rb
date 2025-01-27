f4c2b2d046d9505d1b941e46738596ddb2c5e1cb
borvux <bennyjoram@gmail.com>
2025-01-27 00:40:14 -0500

    asking user for input

--- umbrella.rb
+++ umbrella.rb
@@ -1,4 +1,15 @@
+# rubocop:disable Style/StringLiterals
 # Write your soltuion here!
 require "http"
 require "json"
 require "dotenv/load"
+
+puts "========================================"
+puts "    Will you need an umbrella today?    "
+puts "========================================"
+puts
+print "Where are you: "
+
+location = gets.chomp
+
+puts "Checking the weather at #{location}"
